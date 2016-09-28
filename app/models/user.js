88dc4a1e3b24a139995d3fc2d78570477f3369bd
Saqib <saqib@Saqibs-MacBook-Pro.local>
2016-09-28 02:58:39 -0500

    Added user model

---
+++ app/models/user.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data'
+
+let {
+  Model,
+  attr
+} = DS;
+
+export default Model.extend({
+  uid: attr('string'),
+  username: attr('strig'),
+  name: attr('string'),
+  dob: attr('date'),
+  gender: attr('string')
+})
