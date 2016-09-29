7d875af20ff2c9b0097c25e499a8f2abcd7bdacd
Saqib <saqib@Saqibs-MacBook-Pro.local>
2016-09-28 23:40:07 -0500

    1. Added showing camera and capturing image. 2. Added Creating new Post, and updating to user and in post in firebase. 3. WIP: Upload image somewhere and save the URL.

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,44 @@
 import Ember from 'ember';
+const {get} = Ember;
+function handleVideo(stream) {
+  var video = document.querySelector("#videoElement");
+  console.log(video.clientHeight, video.clientWidth);
+  // video.addEventListener('click', snapshot(stream, video), false);
+  video.src = window.URL.createObjectURL(stream);
+
+}
+
+
+function snapshot() {
+  var canvas = document.querySelector('#canvasCapture');
+  var video = document.querySelector("#videoElement");
+  canvas.getContext('2d').drawImage(video, 0, 0, video.clientHeight, video.clientWidth);
+}
+
+function videoError() {
+  console.log('Error');
+}
 
 export default Ember.Component.extend({
+  didInsertElement: ()=> {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},handleVideo, videoError);
+    }
+  },
+  actions: {
+    capture(){
+      snapshot();
+    },
+    save(){
+      let postTitle = get(this, 'postTitle');
+      this.sendAction('store', postTitle);
+    }
+  },
+  willDestroyElement: function () {
+  var video = document.querySelector("#videoElement");
+  video.src = null;
+}
 });
+
+// let const handleVideo = func
--- app/routes/post/new.js
+++ app/routes/post/new.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
-
+const {get} = Ember;
 export default Ember.Route.extend({
+  actions: {
+    createPost(postTitle){
+      let uid = get(this, 'session.uid');
+      console.log(uid);
+      console.log(postTitle);
+      let post = this.store.createRecord('post', {
+        imageURL: 'http://www.helpinghomelesscats.com/images/cat1.jpg',
+        caption: postTitle
+      })
+
+      this.store.query('user', {orderBy: 'uid', equalTo: uid }).then((records) => {
+        let user = records.get('firstObject');
+
+          user.get('posts').addObject(post)
+          post.save().then(()=> {
+            return user.save();
+          })
+
+          this.transitionTo('feed')
+      })
+
+
+    }
+  }
 });
